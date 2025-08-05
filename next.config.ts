f6be16c34047d8938baea18b661f114a501d5bf8
diemyolo <quydiem2015@gmail.com>
2025-08-05 15:35:03 +0700

    fix image tag

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "blog-api-uploaded-files.s3.amazonaws.com",
+        pathname: "**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
